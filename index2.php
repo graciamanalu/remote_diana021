8f0f5d0987d205c8719ebbb49936c4a2f928ed06
graciamanalu <rosdianagresia@gmail.com>
2024-06-11 15:08:23 +0700

    menambahkan file index2.php

---
+++ index2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index 2 git</title>
+</head>
+<body>
+    <h1>Rosdiana Manalu</h1>
+</body>
+</html>
